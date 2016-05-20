840160e57ee1ecac08d105677ed09d4b77de3bff
vitoo22 <vituz@VitosMacBook.local>
2016-05-20 11:35:43 +0200

    all ok till getting instagram's code

---
+++ get_instagram.py
@@ -0,0 +1,5 @@
+CLIENT_ID = 'acb84376812747f0b6086b14e905332b'
+REDIRECT_URI = 'http://127.0.0.1/authorization-completed/'
+
+
+http://127.0.0.1/authorization-completed?code=4930381800d747b1b5fc3e2be1bb68fc
